e380e3fe1a1d0ed582b30b89dcf2bff8db6a14ac
navaneeth <navaneethprabhupadam@gmail.com>
2024-05-03 23:32:00 +0530

    reg added2

--- estore/website/urls.py
+++ estore/website/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     #path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
-    path('register/', views.regiester_user, name='register'),
+    path('register/', views.register_user, name='register'),
     
 
 ]
--- estore/website/views.py
+++ estore/website/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
+#from .models import Record
 
 # Create your views here.
 def home(request):
@@ -27,5 +29,20 @@ def logout_user(request):
 	return redirect('home')
 
 
-def regiester_user(request):
-	return render(request, 'register.html',{})
+def register_user(request):
+	if request.method == 'POST':
+		form = SignUpForm(request.POST)
+		if form.is_valid():
+			form.save()
+			# Authenticate and login
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, "You Have Successfully Registered! Welcome!")
+			return redirect('home')
+	else:
+		form = SignUpForm()
+		return render(request, 'register.html', {'form':form})
+
+	return render(request, 'register.html', {'form':form})
